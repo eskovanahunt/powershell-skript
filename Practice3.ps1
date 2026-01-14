1f8d5bcca961a7917a2af246408454f2781428aa
Esko Vanahunt <esko.vanahunt@voco.ee>
2026-01-14 13:54:40 +0200

    practice3 green

---
+++ Practice3.ps1
@@ -0,0 +1,10 @@
+﻿#Practice3.
+# Ask a question “What is your name” and then display the answer provided in “Green” color. In case of queries, please check the answer section.
+
+# Ask the user for their name
+
+$name = Read-Host "What is your name"
+
+# Display the name in green color
+
+Write-Host "Hello, $name" -ForegroundColor Green
