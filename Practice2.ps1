b4291a812a15deec1b88fa1fb43491c403b90ec9
Esko Vanahunt <esko.vanahunt@voco.ee>
2026-01-14 13:24:44 +0200

    Added Practice 2 hastable example

---
+++ Practice2.ps1
@@ -0,0 +1,18 @@
+﻿#Practice 2
+#Create 2 hashtables as below and the third hashtable should be calculated as (DaysWorked *SalaryPerDay).
+# Hashtable 1: Days Worked
+$DaysWorked = @{
+    John = 12
+    Joe  = 20
+    Mary = 18
+}
+$SalaryPerDay = @{
+    John = 100
+    Joe  = 120
+    Mary = 150
+}
+$Salary = @{}
+
+foreach ($name in $DaysWorked.Keys) {
+    $Salary[$name] = $DaysWorked[$name] * $SalaryPerDay[$name]
+}
